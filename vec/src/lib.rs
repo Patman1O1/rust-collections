5e3fc064dc46d141617854d5ff5304ef55e99259
Patrick Thomas <patrickthomas101x@gmail.com>
2026-09-23 10:26:51 -0500

    Edit lib.rs

--- vec/src/lib.rs
+++ vec/src/lib.rs
@@ -1,7 +1,35 @@
+// ── Core Aliases ────────────────────────────────────────────────────────────
+use core::{
+    marker::PhantomData,
+    ptr::NonNull
+};
+
+// ── Standard Libary Aliases ─────────────────────────────────────────────────
+
+// ── Self Aliases ────────────────────────────────────────────────────────────
+
+// ── Allocator API 2 Aliases ─────────────────────────────────────────────────
+use allocator_api2::{
+    alloc::{Allocator, Global}
+};
+
 // ── Modules ─────────────────────────────────────────────────────────────────
+// Public
 pub mod drain;
 pub mod extract_if;
 pub mod into_iter;
 pub mod peek_mut;
 pub mod splice;
-pub mod vec;
+
+// Private
+#[cfg(test)]
+mod tests;
+
+// ── `struct Vec<T, A> Definition` ───────────────────────────────────────────
+pub struct Vec<T, A: Allocator = Global> {
+    ptr: NonNull::<T>,
+    cap: usize,
+    len: usize,
+    alloc: A,
+    marker: PhantomData::<T>
+}
